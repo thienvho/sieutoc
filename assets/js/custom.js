b81a4e718f39df86309d666700ea524972826353
Thien Ho <thien@thienho.net>
2020-10-07 13:34:21 -0500

    homepage

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,28 @@
 "use strict";
 
-jQuery(function ($) {});
+jQuery(function ($) {
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    var menu = $(".main-nav");
+
+    if (scroll >= 100) {
+      menu.addClass("__active");
+    } else {
+      menu.removeClass('__active');
+    }
+  }); //missing );
+  //hamburger button toggler
+  // var menu = document.querySelector('#main-nav');
+  // var _toggler = document.querySelector('#hambuger-button');
+  // _toggler.addEventListener('click', function() {
+  //   if(menu.classList.contains('show')) //menu is open
+  //   {
+  //     _toggler.classList.remove('is-active');
+  //     menu.classList.remove('show');
+  //   }
+  //   else {
+  //     _toggler.classList.add('is-active');
+  //     menu.classList.add('show');
+  //   }
+  // });
+});
